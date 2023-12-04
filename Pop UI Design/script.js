4883893b491867851104759a3e4155f7567a51a4
Aditya-Karki03 <adityakarki201@gmail.com>
2023-12-04 10:19:52 +0530

    Completed the POP UI Project

--- Pop UI Design/script.js
+++ Pop UI Design/script.js
@@ -1,10 +1,30 @@
-const button=document.querySelector('button');
-const popUp=document.querySelector('.pop-up');
-const backGround=document.body;
-button.addEventListener('click',()=>{
-    popUp.style.visibility='visible';
-    backGround.style.background='black';
-})
+// const button=document.querySelector('button');
+// const popUp=document.querySelector('.pop-up');
+// const backGround=document.body;
+// button.addEventListener('click',()=>{
+//     popUp.style.visibility='visible';
+//     backGround.style.background='black';
+// })
 // backGround.addEventListener('click',()=>{
 //     popUp.style.visibility='hidden';
 // })
+const button=document.querySelector('button');
+button.addEventListener('click',()=>{
+    buttonToggle();
+})
+
+const backGroundHolder=document.querySelector('.pop-up-background');
+backGroundHolder.addEventListener('click',()=>{
+    buttonToggle();
+})
+const buttonToggle=()=>{
+    const classListInBody=document.body.classList;
+    if(classListInBody.contains('open')){
+        classListInBody.remove('open');
+        classListInBody.add('closed');
+    }
+    else{
+        classListInBody.remove('closed');
+        classListInBody.add('open');
+    }
+}
