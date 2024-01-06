7f121e1098acddc910d596fc7b7471a3f84f2736
Aditya Karki <70797860+Aditya-Karki03@users.noreply.github.com>
2024-01-06 15:24:18 +0530

    Update script.js

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,6 +1,6 @@
 
 async function weather(){
-    const apiURL='https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=107e9ea76e4810e528542eb72b27f7a7';
+   
     const request=new Request(apiURL);
     const response=await fetch(request);
     const WeatherObjText=await response.text();
