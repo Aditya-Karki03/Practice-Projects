6c19fca4d80fa8e8e032e75abd7714239917f526
Aditya-Karki03 <adityakarki201@gmail.com>
2024-02-01 19:32:22 +0530

    Made a clock

---
+++ Clock/script.js
@@ -0,0 +1,27 @@
+const second=document.querySelector('.sec');
+const min=document.querySelector('.min');
+const hrs=document.querySelector('.hour');
+
+let i=1;
+
+const setDate=()=>{
+    const date=new Date();
+    const seconds=date.getSeconds();
+    // if(seconds===60)    second.style.transition='0';
+    
+    console.log(seconds)
+    const secondsInDegrees=Math.floor(((seconds/60)*360)+90);
+    second.style.transform=`rotate(${secondsInDegrees}deg)`
+    // console.log(secondsInDegrees)
+
+    const minutes=date.getMinutes();
+    const MinutesinDegrees=Math.floor(((minutes/60)*360)+90)
+    min.style.transform=`rotate(${MinutesinDegrees}deg)`
+
+    const hour=date.getHours();
+    const hourInDegrees=Math.floor(((hour/(12))*360)+90)
+    hrs.style.transform=`rotate(${hourInDegrees}deg)`
+}
+
+setInterval(setDate,1000)
+
