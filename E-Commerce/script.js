dbce167e76434c1ee7bdd37aea8c502e6c2faf12
Aditya-Karki03 <adityakarki201@gmail.com>
2024-02-16 21:14:56 +0530

    Added some more pages and fixed some issues

--- E-Commerce/script.js
+++ E-Commerce/script.js
@@ -11,7 +11,7 @@ navArrow.addEventListener('click',()=>{
 
 //Listing all the clothes displayed on the webpage
 const clothes=document.querySelectorAll('.image-holder');
-console.log(clothes);
+
 
 //Whenever clicked on each individual clothes it should move to different screen
 clothes.forEach((cloth)=>{
@@ -22,6 +22,77 @@ clothes.forEach((cloth)=>{
     })
 })
 
-function individualProductDisplayer(){
+//Adding cart functionnality
+function AddedToCart(){
+    const carts = document.querySelectorAll('.bxs-cart-add');
+    carts.forEach((cart) => {
+    cart.addEventListener('click', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        
+        cart.classList.toggle('rotated-text');
+        setTimeout(()=>{
+            cart.parentElement.innerHTML="<i class='bx bx-check'></i>";
+            checkedOrNot();
+        },900)
 
+        checkedOrNot();
+    });
+});
+}
+AddedToCart();
+//Once the cloth is added to cart, cart icon changes to tick
+//If clicked on check, I want to change it to cart
+function checkedOrNot(){
+    const checked=document.querySelectorAll('.bx-check');
+checked.forEach((checks)=>{
+    checks.addEventListener('click',(e)=>{
+        e.stopPropagation();
+        e.preventDefault();
+        checks.classList.toggle('anti-rotated-text')
+        setTimeout(()=>{
+            checks.parentElement.innerHTML='<i class="bx bxs-cart-add"></i>';
+            AddedToCart();
+        },900)
+    })
+})
+}
+
+//Lets build star rater
+const divContainingStars=document.querySelectorAll('.stars');
+divContainingStars.forEach((div)=>{
+    div.addEventListener('click',(e)=>{
+        e.stopPropagation();
+        e.preventDefault();
+//Since we've added event listner to a div so we also can click on gaps between the stars
+//Hence to be more precise we are using the if statement
+//This if statement is used to generate index of the selected star.
+//By converting nodelist/HTMLCollection to array
+        if(e.target.classList[0]=='bx'){
+            const parentEle=e.target.parentElement;
+            const arrayOfStars=Array.from(parentEle.children);
+            const index=arrayOfStars.indexOf(e.target);
+            starMaker(index,arrayOfStars);
+        }
+    })
+    
+})
+
+//It colors the stars depending on which star is being clicked
+function starMaker(index,arrayOfStars){
+    const computedStyle = window.getComputedStyle(arrayOfStars[index]);
+    const color = computedStyle.color;
+    if(index<4){
+        for(let i=index+1;i<=4;i++){
+            arrayOfStars[i].style.color='black';
+        }
+        for(let i=0;i<=index;i++){
+            arrayOfStars[i].style.color='rgb(253,184,19)';
+        }
+    }
+    if(index==4){
+        for(let i=0;i<=index;i++){
+            arrayOfStars[i].style.color='rgb(253,184,19)';
+        }
+    }
 }
